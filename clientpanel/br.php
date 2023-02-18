ae21427db797631b84033a8a82a499a466660beb
ely <48741697+elyrodso@users.noreply.github.com>
2023-02-18 02:27:24 -0300

    Create br.php
    Brazilian Portuguese support.

---
+++ clientpanel/br.php
@@ -0,0 +1,88 @@
+<?php
+
+// navigation bar
+$lang['nav']['dashboard'] = "Painel";
+$lang['nav']['coins'] = "Moedas";
+$lang['nav']['mainpage'] = "Página Inicial";
+$lang['nav']['servers'] = "Servidores";
+$lang['nav']['createserver'] = "Criar servidor";
+$lang['nav']['changeram'] = "Mudar RAM";
+$lang['nav']['alwaysonline'] = "Sempre Online";
+$lang['nav']['soon'] = "Em breve";
+$lang['nav']['account'] = "Conta";
+$lang['nav']['logintopanel'] = "Entrar no Painel";
+$lang['nav']['plandetails'] = "Detalhes do plano";
+$lang['nav']['billing'] = "Pagamento";
+
+// notices
+$lang['notices']['premiumdiscount'] = "Consiga <b>10% de desconto</b> por <b>6 meses</b> com o código de cupom <b>UPGRADE10</b> ao atualizar seu plano para o Power e o Ultimate.";
+$lang['notices']['premiumad'] = "Hospede seu servidor em hardware de alto nível com recursos adicionais como online 24/7 e sem anúncios com nossos Planos Falix Premium por preços abaixo do mercado. <a href='https://falixnodes.net/minecraft-server-hosting'>Clique aqui</a>!";
+
+
+// quick links
+$lang['links']['quicklinks'] = "Links de acesso rápido";
+$lang['links']['quicklinks_description'] = "Navegue através do painel com nossos links de acesso rápido";
+$lang['links']['createserver'] = "Crie seu próprio servidor";
+$lang['links']['createserver_description'] = "Crie seu próprio servidor Minecraft";
+$lang['links']['coinshop'] = "Comprar em Loja FalixCoins";
+$lang['links']['coinshop_description'] = "Compre boosts para seu servidor Minecraft";
+$lang['links']['coins'] = "Ganhe FalixCoins de graça";
+$lang['links']['coins_description'] = "Impulsione seu servidor ao ganhar nossa moeda virtual";
+$lang['links']['discordsupport'] = "Suporte por Discord";
+$lang['links']['discordsupport_description'] = "Faça suas perguntas à nossa comunidade";
+
+// resources widget
+$lang['resources']['serversbalance'] = "Saldo dos Servidores";
+$lang['resources']['rambalance'] = "RAM Saldo";
+$lang['resources']['cpubalance'] = "CPU Saldo";
+$lang['resources']['diskbalance'] = "Disco Saldo";
+$lang['resources']['databasesbalance'] = "Databases Saldo";
+$lang['resources']['portsbalance'] = "Portas Saldo";
+
+// index.php / main page
+$lang['manage_servers'] = "Controle seus servidores com nosso painel de jogos de fácil utilização.";
+
+// servers table
+$lang['servers'] = "Seus Servidores";
+$lang['logintogamepanel'] = "Entrar no Painel de Jogos";
+
+$lang['server']['name'] = "NOME DO SERVIDOR";
+$lang['server']['cpu'] = "CPU";
+$lang['server']['ram'] = "RAM";
+$lang['server']['disk'] = "DISCO";
+$lang['server']['database'] = "DATABASE";
+$lang['server']['port'] = "PORTAS";
+
+$lang['server']['gotoserver'] = "Vá para Servidor";
+$lang['server']['changecpu'] = "Alterar CPU";
+$lang['server']['changeram'] = "Alterar RAM";
+$lang['server']['changedisk'] = "Alterar Disco";
+$lang['server']['deleteserver'] = "Apagadar Servidor";
+
+// create server page
+$lang['create']['createserver'] = "Criar Servidor";
+$lang['create']['servername'] = "Nome do Servidor";
+$lang['create']['serverram'] = "RAM do Servidor";
+$lang['create']['servergame'] = "Jogo";
+$lang['create']['allgames'] = "Todos os jogos suportados";
+$lang['create']['selectgame'] = "Selecionar o jogo";
+$lang['create']['minecrafteula'] = "Eu aceito os <a href='https://account.mojang.com/documents/minecraft_eula/'>Contrato de Licença de Usuário Final do Minecraft (EULA)</a>";
+$lang['create']['createbutton'] = "Criar";
+
+// coins page
+$lang['coins']['earnfalixcoins'] = "Ganhe FalixCoins";
+$lang['coins']['resourcestore'] = "Loja FalixCoins";
+$lang['coins']['task'] = "Tarefa";
+$lang['coins']['reward'] = "Recompensa";
+$lang['coins']['earn'] = "Ganhe";
+$lang['coins']['claim'] = "Colete";
+$lang['coins']['invitefriends'] = "Convide seus amigos";
+$lang['coins']['joinforrewards'] = "Entre por recompensas";
+$lang['coins']['invitereward'] = "1,000 FalixCoins por amigo convidado";
+$lang['coins']['joinreward'] = "até 50 FalixCoins por servidor entrado";
+
+// affiliate page
+$lang['affiliate']['title'] = "Convide seus amigos e ganhe 1,000 FalixCoins";
+$lang['affiliate']['description'] = "Convide seus amigos e consiga sua recompensa em segundos.";
+$lang['affiliate']['seconddescription'] = "Convide seus amigos ao copiar o link do website abaixo e compartilhe para seus amigos.";
+$lang['affiliate']['footer'] = "Depois de convidar amigos, você poderá <a href='https://client.falixnodes.net/affiliate/status'>ver o status de suas indicações</a> ou entrar em contato conosco em nosso servidor Discord se você tiver alguma dúvida.";
