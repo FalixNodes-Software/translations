4935024dd6e59e05e1e5bc0915e2ed61bcaee3ac
Lilja <60701240+LiljaFi@users.noreply.github.com>
2023-02-18 04:08:49 +0200

    Create en.php

---
+++ clientpanel/en.php
@@ -0,0 +1,53 @@
+<?php
+
+// notices
+$lang['notices']['premiumdiscount'] = "Get <b>10% discount</b> for <b>6 months</b> with coupon code <b>UPGRADE10</b> when upgrading your plan to Power and Ultimate.";
+$lang['notices']['premiumad'] = "Host your server on top notch hardware with added features like 24/7 online and ad-free website with our Falix Premium Plans for below market prices. <a href='https://falixnodes.net/minecraft-server-hosting'>Click me</a>!";
+
+
+// quick links
+$lang['links']['quicklinks'] = "Quick Links";
+$lang['links']['quicklinks_description'] = "Navigate through the dashboard with our quick links";
+$lang['links']['createserver'] = "Create your own Server";
+$lang['links']['createserver_description'] = "Create your own Minecraft server";
+$lang['links']['coinshop'] = "Shop in FalixCoins Store";
+$lang['links']['coinshop_description'] = "Purchase boosts for your Minecraft server";
+$lang['links']['coins'] = "Earn FalixCoins for free";
+$lang['links']['coins_description'] = "Boost your server by earning our virtual currency";
+$lang['links']['discordsupport'] = "Discord Support";
+$lang['links']['discordsupport_description'] = "Ask your questions to our community";
+
+// resources widget
+$lang['resources']['serversbalance'] = "Servers Balance";
+$lang['resources']['rambalance'] = "RAM Balance";
+$lang['resources']['cpubalance'] = "CPU Balance";
+$lang['resources']['diskbalance'] = "Disk Balance";
+$lang['resources']['databasesbalance'] = "Databases Balance";
+$lang['resources']['portsbalance'] = "Ports Balance";
+
+// index.php / main page
+$lang['manage_servers'] = "Control your servers with our user friendly game panel.";
+
+// servers table
+$lang['servers'] = "Your Servers";
+$lang['logintogamepanel'] = "Login to Game Panel";
+
+$lang['server']['name'] = "SERVER NAME";
+$lang['server']['cpu'] = "CPU";
+$lang['server']['ram'] = "RAM";
+$lang['server']['disk'] = "DISK";
+$lang['server']['database'] = "DATABASE";
+$lang['server']['port'] = "PORTS";
+
+$lang['server']['gotoserver'] = "Go to Server";
+$lang['server']['changecpu'] = "Change CPU";
+$lang['server']['changeram'] = "Change RAM";
+$lang['server']['changedisk'] = "Change Disk";
+$lang['server']['deleteserver'] = "Delete Server";
+
+// create server page
+$lang['create']['createserver'] = "Create Server";
+$lang['create']['servername'] = "Server Name";
+$lang['create']['serverram'] = "Server RAM";
+$lang['create']['minecrafteula'] = "I accept the <a href='https://account.mojang.com/documents/minecraft_eula/'>Minecraft End User License Agreement (EULA)</a>";
+$lang['create']['createbutton'] = "Create";
