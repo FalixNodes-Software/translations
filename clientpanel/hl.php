37ea0df93cdbb77b681f695f728a9e957c3e2848
Samplayz2007 <itsmesamuel.2007@gmail.com>
2023-02-20 21:30:05 +0530

    Added Hindi Translations

---
+++ clientpanel/hl.php
@@ -0,0 +1,158 @@
+﻿<?php
+
+// navigation bar
+$lang['nav']['dashboard'] = "डैशबोर्ड";
+$lang['nav']['coins'] = "सिक्के";
+$lang['nav']['mainpage'] = "मुख्य पृष्ठ";
+$lang['nav']['servers'] = "सर्वर";
+$lang['nav']['createserver'] = "अपना सर्वर बनाएं";
+$lang['nav']['changeram'] = "अपना राम बदलो";
+$lang['nav']['alwaysonline'] = "हमेशा ऑनलाइन (कार्य प्रगति पर है)";
+$lang['nav']['soon'] = "जल्दी";
+$lang['nav']['account'] = "खाता प्रबंधन";
+$lang['nav']['logintopanel'] = "कंट्रोल पैनल में लॉग इन करें";
+$lang['nav']['plandetails'] = "सदस्यता विवरण";
+$lang['nav']['billing'] = "भुगतान प्रबंधन";
+
+// notices
+$lang['notices']['premiumdiscount'] = "अपने प्लान को पावर और अल्टीमेट में अपग्रेड करते समय कूपन कोड<b> UPGRADE10</b> के साथ <b>6 महीने</b> के लिए <b>10% छूट</b> प्राप्त करें।";
+$lang['notices']['premiumad'] = "24/7 ऑनलाइन और विज्ञापन-मुक्त वेबसाइट जैसी अतिरिक्त सुविधाओं के साथ अपने सर्वर को शीर्ष पायदान हार्डवेयर पर हमारे फेलिक्स प्रीमियम प्लान के साथ बाजार मूल्य से कम पर होस्ट करें। <a href='https://falixnodes.net/minecraft-server-hosting'>मुझे क्लिक करें</a>!";
+
+// widgets
+$lang['widgets']['panel_title'] = "नियंत्रण कक्ष लॉगिन विवरण";
+$lang['widgets']['panel_ftp'] = "आपका FTP पासवर्ड";
+$lang['widgets']['panel_resetpass'] = "गलत पासवर्ड? में अपना पासवर्ड रीसेट कर सकते हैं <a href='/profile/settings'>profile settings</a>.";
+$lang['widgets']['panel_loginbutton'] = "कंट्रोल पैनल में लॉग इन करें";
+$lang['widgets']['panel_closebutton'] = "Close";
+
+$lang['widgets']['changecpu_title'] = "सीपीयू बदलें";
+$lang['widgets']['changecpu_newcpu'] = "नया सर्वर सीपीयू कोर";
+$lang['widgets']['changecpu_changebutton'] = "सीपीयू बदलें";
+$lang['widgets']['changecpu_closebutton'] = "Close";
+
+$lang['widgets']['changedisk_title'] = "अपना डिस्क स्थान आवंटन बदलें";
+$lang['widgets']['changedisk_newcpu'] = "नया सर्वर डिस्क स्थान (MB में)";
+$lang['widgets']['changedisk_changebutton'] = "परिवर्तन";
+$lang['widgets']['changedisk_closebutton'] = "Close";
+
+// quick links
+$lang['links']['quicklinks'] = "त्वरित सम्पक";
+$lang['links']['quicklinks_description'] = "हमारे त्वरित लिंक के साथ डैशबोर्ड में नेविगेट करें";
+$lang['links']['createserver'] = "अपना खुद का सर्वर बनाएं";
+$lang['links']['createserver_description'] = "Create your own Minecraft server";
+$lang['links']['coinshop'] = "Falix कॉइन स्टोर में खरीदारी करें";
+$lang['links']['coinshop_description'] = "आपके Minecraft सर्वर के लिए खरीद संसाधन बढ़ा देता है";
+$lang['links']['coins'] = "Falix क्सिक्के मुफ़्त कमाएँ";
+$lang['links']['coins_description'] = "हमारी आभासी मुद्रा अर्जित करके अपने सर्वर को बूस्ट करें";
+$lang['links']['discordsupport'] = "Discord सहायता";
+$lang['links']['discordsupport_description'] = "अपने प्रश्न हमारे समुदाय से पूछें";
+
+// resources widget
+$lang['resources']['serversbalance'] = "सर्वर स्लॉट बैलेंस";
+$lang['resources']['rambalance'] = "RAM संतुलन";
+$lang['resources']['cpubalance'] = "CPU संतुलन";
+$lang['resources']['diskbalance'] = "Disk संतुलन";
+$lang['resources']['databasesbalance'] = "Databases संतुलन";
+$lang['resources']['portsbalance'] = "Ports संतुलन";
+
+// index.php / main page
+$lang['manage_servers'] = "हमारे उपयोगकर्ता के अनुकूल गेम पैनल के साथ अपने सर्वर को नियंत्रित करें।";
+
+// servers table
+$lang['servers'] = "Your Servers";
+$lang['logintogamepanel'] = "आपके सर्वर";
+
+$lang['server']['name'] = "सर्वर का नाम";
+$lang['server']['cpu'] = "CPU";
+$lang['server']['ram'] = "RAM";
+$lang['server']['disk'] = "DISK";
+$lang['server']['database'] = "DATABASE";
+$lang['server']['port'] = "PORTS";
+
+$lang['server']['gotoserver'] = "सर्वर प्रबंधित करें";
+$lang['server']['changecpu'] = "परिवर्तन CPU";
+$lang['server']['changeram'] = "परिवर्तन RAM";
+$lang['server']['changedisk'] = "परिवर्तन Disk";
+$lang['server']['deleteserver'] = "सर्वर हटाएं";
+
+// create server page
+$lang['create']['createserver'] = "सर्वर बनाएँ";
+$lang['create']['servername'] = "सर्वर का नाम";
+$lang['create']['serverram'] = "सर्वर रैम";
+$lang['create']['servergame'] = "Game";
+$lang['create']['allgames'] = "सभी समर्थित खेल";
+$lang['create']['selectgame'] = "खेल का चयन करें";
+$lang['create']['minecrafteula'] = "मुझे स्वीकार है<a href='https://account.mojang.com/documents/minecraft_eula/'>Minecraft अंत उपयोगकर्ता लाइसेंस समझौता (EULA)</a>";
+$lang['create']['createbutton'] = "Create";
+
+// change ram page
+$lang['changeram']['title'] = "रैम बदलें";
+$lang['changeram']['servername'] = "सर्वर का नाम";
+$lang['changeram']['currentram'] = "वर्तमान रैम";
+$lang['changeram']['newram'] = "नई रैम राशि";
+$lang['changeram']['changerambutton'] = "रैम बदलें";
+
+// coins page
+$lang['coins']['earnfalixcoins'] = "Falix सिक्के कमाएँ";
+$lang['coins']['resourcestore'] = "Falix सिक्के दुकान";
+$lang['coins']['task'] = "काम";
+$lang['coins']['reward'] = "पुरस्कार";
+$lang['coins']['earn'] = "कमाना";
+$lang['coins']['claim'] = "दावा";
+$lang['coins']['invitefriends'] = "अपने दोस्तों को आमंत्रित करो";
+$lang['coins']['joinforrewards'] = "अपने दोस्तों को आमंत्रित करें";
+$lang['coins']['invitereward'] = "1,000 Falix प्रति आमंत्रित मित्र के सिक्के";
+$lang['coins']['joinreward'] = "तक 50 Falix प्रत्येक सर्वर से जुड़े सिक्के";
+
+// affiliate page
+$lang['affiliate']['title'] = "एक दोस्त को आमंत्रित करें और प्राप्त करें 1,000 Falix सिक्के";
+$lang['affiliate']['description'] = "अपने दोस्तों को आमंत्रित करें और सेकंड में अपना इनाम प्राप्त करें।";
+$lang['affiliate']['seconddescription'] = "नीचे दिए गए वेबसाइट लिंक को कॉपी करके अपने दोस्तों को आमंत्रित करें और अपने दोस्तों को भेजें।";
+$lang['affiliate']['footer'] = "दोस्तों को आमंत्रित करने के बाद, आप कर सकते हैं<a href='https://client.falixnodes.net/affiliate/status'>अपने रेफरल की स्थिति देखें </a> या यदि आपके कोई प्रश्न हैं तो हमारे डिस्कॉर्ड सर्वर पर हमसे संपर्क करें।";
+
+// activity page
+$lang['activity']['title'] = "गतिविधि";
+$lang['activity']['id'] = "पहचान";
+$lang['activity']['action'] = "ACTION";
+$lang['activity']['description'] = "विवरण";
+$lang['activity']['useragent'] = "उपयोगकर्ता एजेंट";
+$lang['activity']['ip'] = "आईपी ​​पता";
+$lang['activity']['date'] = "तारीख";
+
+// profile page
+$lang['profile']['settings_title'] = "पार्श्वचित्र समायोजन";
+$lang['profile']['firstname'] = "पहला नाम";
+$lang['profile']['lastname'] = "उपनाम";
+$lang['profile']['email'] = "ईमेल";
+$lang['profile']['theme'] = "वेबसाइट थीम";
+$lang['profile']['textcolor'] = "पाठ का रंग";
+
+$lang['profile']['serversettings_title'] = "सर्वर सेटिंग्स";
+$lang['profile']['externalserverstart'] = "बाहरी सर्वर प्रारंभ";
+$lang['profile']['update'] = "अद्यतन";
+$lang['profile']['enable'] = "सक्षम";
+$lang['profile']['disable'] = "अक्षम करना";
+
+$lang['profile']['emailpreference_title'] = "ईमेल वरीयता";
+$lang['profile']['emailpreference'] = "अपनी ईमेल वरीयता बदलें";
+
+$lang['profile']['changepassword_title'] = "पासवर्ड बदलें";
+$lang['profile']['changepassword_notice'] = "नायब! यदि आप गेम पैनल पासवर्ड बदलने की कोशिश कर रहे हैं, तो दूसरे पासवर्ड प्रॉम्प्ट पर नीचे स्क्रॉल करें।";
+$lang['profile']['oldpassword'] = "पुराना पासवर्ड";
+$lang['profile']['newpassword'] = "नया पासवर्ड";
+$lang['profile']['confirmpassword'] = "पासवर्ड की पुष्टि कीजिये";
+$lang['profile']['updatepassword'] = "पासवर्ड अपडेट करें";
+
+$lang['profile']['discord'] = "Discord";
+$lang['profile']['authorised'] = "के रूप में प्राधिकृत";
+$lang['profile']['linkdiscord'] = "जोड़ना Discord";
+$lang['profile']['unlinkdiscord'] = "उतारना Discord";
+$lang['profle']['notlinked'] = "आपके पास नहीं है Discord account अपने लिंक करने के लिए, खाते से लिंक किया गया Discord account, अपने को अधिकृत करें Discord account";
+
+$lang['profile']['dangerzone'] = "खतरा क्षेत्र";
+$lang['profile']['changegamepanelpassword_title'] = "गेम पैनल का पासवर्ड बदलें";
+$lang['profile']['changegamepanelpassword_description'] = "गेम पैनल पासवर्ड बदलने के बाद, पुराना पासवर्ड काम नहीं करेगा।";
+$lang['profile']['changegamepanelpassword_button'] = "पासवर्ड बदलें";
+$lang['profile']['deleteaccount_title'] = "इस खाते को हटा दें";
+$lang['profile']['deleteaccount_description'] = "एक बार जब आप खाता हटा देते हैं, तो वापस नहीं जा रहा है। कृपया निश्चिंत रहें।";
+$lang['profile']['deleteaccount_button'] = "खाता हटा दो";
