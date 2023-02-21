94044d6f680ec6834e2aafd22c76c61716e3f226
Samplayz2007 <itsmesamuel.2007@gmail.com>
2023-02-21 13:01:39 +0530

    Added Tamil And Telugu Translations

---
+++ clientpanel/ta.php
@@ -0,0 +1,158 @@
+﻿<?php
+
+// navigation bar
+$lang['nav']['dashboard'] = "டாஷ்போர்டு";
+$lang['nav']['coins'] = "நாணய";
+$lang['nav']['mainpage'] = "முதன்மை பக்கம்";
+$lang['nav']['servers'] = "சேவையகங்கள்";
+$lang['nav']['createserver'] = "சேவையகத்தை உருவாக்கவும்";
+$lang['nav']['changeram'] = "ரேம் அளவை மாற்றவும்";
+$lang['nav']['alwaysonline'] = "எப்போதும் ஆன்லைன்";
+$lang['nav']['soon'] = "விரைவில்";
+$lang['nav']['account'] = "Account";
+$lang['nav']['logintopanel'] = "கண்ட்ரோல் பேனலைத் திறக்கவும்";
+$lang['nav']['plandetails'] = "சந்தா விவரங்கள்";
+$lang['nav']['billing'] = "பில்லிங்";
+
+// notices
+$lang['notices']['premiumdiscount'] = "உங்கள் திட்டத்தை பவர் மற்றும் அல்டிமேட்டிற்கு மேம்படுத்தும் போது <b>UPGRADE10</b> என்ற கூப்பன் குறியீட்டுடன் <b>6 மாதங்களுக்கு</b> <b>10% தள்ளுபடியைப்</b> பெறுங்கள்.";
+$lang['notices']['premiumad'] = "24/7 ஆன்லைன் மற்றும் விளம்பரமில்லா இணையதளம் போன்ற கூடுதல் அம்சங்களுடன் உங்கள் சேவையகத்தை சிறந்த வன்பொருளில் ஹோஸ்ட் செய்யவும், சந்தை விலைக்குக் குறைவான விலையில் எங்கள் Falix பிரீமியம் திட்டங்களுடன். <a href='https://falixnodes.net/minecraft-server-hosting'>என்னைக் கிளிக் செய்யவும்</a>!";
+
+// widgets
+$lang['widgets']['panel_title'] = "உங்கள் பேனல் விவரங்கள்";
+$lang['widgets']['panel_ftp'] = "உங்கள் FTP கடவுச்சொல்";
+$lang['widgets']['panel_resetpass'] = "தவறான கடவுச்சொல்? உங்கள் <a href='/profile/settings'>சுயவிவர அமைப்புகளில்</a> உங்கள் கடவுச்சொல்லை மீட்டமைக்கலாம்.";
+$lang['widgets']['panel_loginbutton'] = "கண்ட்ரோல் பேனலைத் திறக்கவும்";
+$lang['widgets']['panel_closebutton'] = "Close";
+
+$lang['widgets']['changecpu_title'] = "CPU ஐ மாற்றவும்";
+$lang['widgets']['changecpu_newcpu'] = "புதிய சர்வர் CPU கோர்கள்";
+$lang['widgets']['changecpu_changebutton'] = "CPU ஐ மாற்றவும்";
+$lang['widgets']['changecpu_closebutton'] = "Close";
+
+$lang['widgets']['changedisk_title'] = "Disk மாற்றவும்";
+$lang['widgets']['changedisk_newcpu'] = "புதிய சர்வர் வட்டு இடம் (MB இல்)";
+$lang['widgets']['changedisk_changebutton'] = "Disk மாற்றவும்";
+$lang['widgets']['changedisk_closebutton'] = "Close";
+
+// quick links
+$lang['links']['quicklinks'] = "விரைவு இணைப்புகள்";
+$lang['links']['quicklinks_description'] = "எங்களின் விரைவு இணைப்புகள் மூலம் டாஷ்போர்டு வழியாக செல்லவும்";
+$lang['links']['createserver'] = "உங்கள் சொந்த சேவையகத்தை உருவாக்கவும்";
+$lang['links']['createserver_description'] = "உங்கள் சொந்த Minecraft சேவையகத்தை உருவாக்கவும்";
+$lang['links']['coinshop'] = "FalixCoins ஸ்டோரில் ஷாப்பிங் செய்யுங்கள்";
+$lang['links']['coinshop_description'] = "உங்கள் Minecraft சேவையகத்திற்கான கொள்முதல் ஊக்கமளிக்கிறது";
+$lang['links']['coins'] = "FalixCoins ஐ இலவசமாகப் பெறுங்கள்";
+$lang['links']['coins_description'] = "எங்கள் மெய்நிகர் நாணயத்தைப் பெறுவதன் மூலம் உங்கள் சேவையகத்தை அதிகரிக்கவும்";
+$lang['links']['discordsupport'] = "Discord ஆதரவு";
+$lang['links']['discordsupport_description'] = "உங்கள் கேள்விகளை எங்கள் சமூகத்திடம் கேளுங்கள்";
+
+// resources widget
+$lang['resources']['serversbalance'] = "சேவையக இருப்பு";
+$lang['resources']['rambalance'] = "RAM இருப்பு";
+$lang['resources']['cpubalance'] = "CPU இருப்பு";
+$lang['resources']['diskbalance'] = "Disk இருப்பு";
+$lang['resources']['databasesbalance'] = "Databases இருப்பு";
+$lang['resources']['portsbalance'] = "Ports இருப்பு";
+
+// index.php / main page
+$lang['manage_servers'] = "எங்கள் பயனர் நட்பு கேம் பேனல் மூலம் உங்கள் சர்வர்களைக் கட்டுப்படுத்தவும்.";
+
+// servers table
+$lang['servers'] = "உங்கள் சேவையகங்கள்";
+$lang['logintogamepanel'] = "உங்கள் Game பேனலைத் திறக்கவும்";
+
+$lang['server']['name'] = "உங்கள் சர்வர் பெயர்";
+$lang['server']['cpu'] = "CPU";
+$lang['server']['ram'] = "RAM";
+$lang['server']['disk'] = "DISK";
+$lang['server']['database'] = "DATABASE";
+$lang['server']['port'] = "PORTS";
+
+$lang['server']['gotoserver'] = "உங்கள் சேவையகத்தை நிர்வகிக்கவும்";
+$lang['server']['changecpu'] = "CPU ஐ மாற்றவும்";
+$lang['server']['changeram'] = "RAM ஐ மாற்றவும்";
+$lang['server']['changedisk'] = "Disk ஐ மாற்றவும்";
+$lang['server']['deleteserver'] = "சேவையகத்தை நீக்கு";
+
+// create server page
+$lang['create']['createserver'] = "சேவையகத்தை உருவாக்கவும்";
+$lang['create']['servername'] = "சர்வர் பெயர்";
+$lang['create']['serverram'] = "சர்வர் ரேம்";
+$lang['create']['servergame'] = "Game";
+$lang['create']['allgames'] = "அனைத்து ஆதரிக்கப்படும் games";
+$lang['create']['selectgame'] = "game ஐ தேர்ந்தெடுக்கவும்";
+$lang['create']['minecrafteula'] = "நான் <a href='https://account.mojang.com/documents/minecraft_eula/'>Minecraft இறுதி பயனர் உரிம ஒப்பந்தத்தை ஏற்கிறேன்(EULA)</a>";
+$lang['create']['createbutton'] = "உருவாக்கு";
+
+// change ram page
+$lang['changeram']['title'] = "ரேமை மாற்றவும்";
+$lang['changeram']['servername'] = "சர்வர் பெயர்";
+$lang['changeram']['currentram'] = "தற்போதைய ரேம்";
+$lang['changeram']['newram'] = "புதிய ரேம்";
+$lang['changeram']['changerambutton'] = "ரேமை மாற்றவும்";
+
+// coins page
+$lang['coins']['earnfalixcoins'] = "FalixCoins சம்பாதிக்கவும்";
+$lang['coins']['resourcestore'] = "FalixCoins கடை";
+$lang['coins']['task'] = "பணி";
+$lang['coins']['reward'] = "வெகுமதி";
+$lang['coins']['earn'] = "சம்பாதி";
+$lang['coins']['claim'] = "பெறு";
+$lang['coins']['invitefriends'] = "உங்கள் நண்பர்களை அழைக்கவும்";
+$lang['coins']['joinforrewards'] = "வெகுமதிகளுக்காக சேரவும்";
+$lang['coins']['invitereward'] = "அழைக்கப்பட்ட நண்பருக்கு 1,000 FalixCoins";
+$lang['coins']['joinreward'] = "ஒரு இணைந்த சேவையகத்திற்கு 50 FalixCoins வரை";
+
+// affiliate page
+$lang['affiliate']['title'] = "நண்பரை அழைத்து 1,000 FalixCoins ஐப் பெறுங்கள்";
+$lang['affiliate']['description'] = "உங்கள் நண்பர்களை அழைத்து நொடிகளில் வெகுமதியைப் பெறுங்கள்.";
+$lang['affiliate']['seconddescription'] = "கீழே உள்ள இணையதள இணைப்பை நகலெடுத்து உங்கள் நண்பர்களுக்கு அனுப்புவதன் மூலம் உங்கள் நண்பர்களை அழைக்கவும்.";
+$lang['affiliate']['footer'] = "நீங்கள் நண்பர்களை அழைத்தவுடன், <a href='https://client.falixnodes.net/affiliate/status'>உங்கள் பரிந்துரைகளின் நிலையைப் பார்க்கலாம்</a> அல்லது உங்களிடம் ஏதேனும் இருந்தால், எங்கள் டிஸ்கார்ட் சேவையகத்தில் எங்களைத் தொடர்புகொள்ளலாம் கேள்விகள்.";
+
+// activity page
+$lang['activity']['title'] = "நடவடிக்கைகள்";
+$lang['activity']['id'] = "அடையாளம்";
+$lang['activity']['action'] = "நடவடிக்கை";
+$lang['activity']['description'] = "விளக்கம்";
+$lang['activity']['useragent'] = "USER-AGENT";
+$lang['activity']['ip'] = "ஐபி முகவரி";
+$lang['activity']['date'] = "தேதி";
+
+// profile page
+$lang['profile']['settings_title'] = "சுயவிவர அமைப்புகள்";
+$lang['profile']['firstname'] = "முதல் பெயர்";
+$lang['profile']['lastname'] = "கடைசி பெயர்";
+$lang['profile']['email'] = "மின்னஞ்சல்";
+$lang['profile']['theme'] = "Website Theme";
+$lang['profile']['textcolor'] = "உரை நிறம்";
+
+$lang['profile']['serversettings_title'] = "சேவையக அமைப்புகள்";
+$lang['profile']['externalserverstart'] = "வெளிப்புற சேவையக தொடக்கம்";
+$lang['profile']['update'] = "புதுப்பிக்கவும்";
+$lang['profile']['enable'] = "இயக்கு";
+$lang['profile']['disable'] = "முடக்கு";
+
+$lang['profile']['emailpreference_title'] = "மின்னஞ்சல் விருப்பம்";
+$lang['profile']['emailpreference'] = "உங்கள் மின்னஞ்சல் விருப்பத்தை மாற்றவும்";
+
+$lang['profile']['changepassword_title'] = "கடவுச்சொல்லை மாற்று";
+$lang['profile']['changepassword_notice'] = "NB! நீங்கள் கேம் பேனல் கடவுச்சொல்லை மாற்ற முயற்சிக்கிறீர்கள் என்றால், மற்ற கடவுச்சொல் வரியில் கீழே உருட்டவும்.";
+$lang['profile']['oldpassword'] = "பழைய கடவுச்சொல்";
+$lang['profile']['newpassword'] = "புதிய கடவுச்சொல்";
+$lang['profile']['confirmpassword'] = "கடவுச்சொல்லை உறுதிப்படுத்தவும்";
+$lang['profile']['updatepassword'] = "கடவுச்சொல்லை புதுப்பிக்கவும்";
+
+$lang['profile']['discord'] = "Discord";
+$lang['profile']['authorised'] = "என அங்கீகரிக்கப்பட்டது";
+$lang['profile']['linkdiscord'] = "இணைப்பு டிஸ் Discord";
+$lang['profile']['unlinkdiscord'] = "இணைப்பை நீக்கவும் Discord";
+$lang['profle']['notlinked'] = "உன்னிடம் இல்லை Discord கணக்குடன் இணைக்கப்பட்ட கணக்கு, உங்கள் Discord கணக்கு, உங்கள் அங்கீகாரம் Discord கணக்கு";
+
+$lang['profile']['dangerzone'] = "ஆபத்து மண்டலம்";
+$lang['profile']['changegamepanelpassword_title'] = "Game Panel's கடவுச்சொல்லை மாற்றவும்";
+$lang['profile']['changegamepanelpassword_description'] = "கேம் பேனல் கடவுச்சொல்லை மாற்றினால், பழைய கடவுச்சொல் வேலை செய்யாது.";
+$lang['profile']['changegamepanelpassword_button'] = "கடவுச்சொல்லை மாற்று";
+$lang['profile']['deleteaccount_title'] = "இந்தக் கணக்கை நீக்கவும்";
+$lang['profile']['deleteaccount_description'] = "ஒரு கணக்கை நீக்கிவிட்டால், பின்வாங்க முடியாது. தயவுசெய்து உறுதியாக இருங்கள்.";
+$lang['profile']['deleteaccount_button'] = "கணக்கை நீக்குக";
