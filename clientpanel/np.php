a96eb6c576dcded39dc6f7ed56e008acc5efa0c9
Susan Bhattarai <83157048+ImmaSB@users.noreply.github.com>
2023-03-07 22:35:47 +0545

    Added Nepali Translation

---
+++ clientpanel/np.php
@@ -0,0 +1,269 @@
+<?php
+
+// page titles
+$lang['title']['mainpage_title'] = "सर्भरहरू हेर्नुहोस्";
+$lang['title']['mainpage_description'] = "तपाईंको Minecraft सर्भरहरू हेर्नुहोस् र व्यवस्थापन गर्नुहोस्।";
+
+$lang['title']['createserver_title'] = "सर्भर सिर्जना गर्नुहोस्";
+$lang['title']['createserver_description'] = "तपाईंको Minecraft सर्भर सिर्जना गर्नुहोस्।";
+
+$lang['title']['changeram_title'] = "RAM परिवर्तन गर्नुहोस्";
+$lang['title']['changeram_description'] = "तपाईंको सर्भर स्रोतहरू परिवर्तन गर्नुहोस्।";
+
+$lang['title']['alwaysonline_title'] = "सधैं अनलाइन";
+$lang['title']['alwaysonline_description'] = "काम प्रगतिमा छ।";
+
+$lang['title']['coins_title'] = "सिक्का";
+$lang['title']['coins_description'] = "सिक्का प्राप्त गर्नुहोस् र Minecraft सर्भर बूस्टहरूको लागि तिनीहरूलाई साट्नुहोस्।";
+
+// navigation page
+$lang['nav']['dashboard'] = "ड्यासबोर्ड";
+$lang['nav']['coins'] = "सिक्का";
+$lang['nav']['mainpage'] = "मुख्य पृष्ठ";
+$lang['nav']['servers'] = "सर्भर";
+$lang['nav']['createserver'] = "सर्भर सिर्जना गर्नुहोस्";
+$lang['nav']['changeram'] = "RAM परिवर्तन गर्नुहोस्";
+$lang['nav']['alwaysonline'] = "सधैँ सक्रिय";
+$lang['nav']['soon'] = "चाँडै";
+$lang['nav']['account'] = "खाता";
+$lang['nav']['logintopanel'] = "प्यानलमा लगइन गर्नुहोस्";
+$lang['nav']['plandetails'] = "योजना विवरणहरू हेर्नुहोस्";
+$lang['nav']['billing'] = "बिलिंग";
+
+// errors
+$lang['error']['login_is_required'] = "लगइन आवश्यक छ";
+$lang['error']['login_is_required_description'] = "तपाईंको सर्भरहरू हेर्न लगइन गर्नुहोस्।";
+$lang['error']['login_is_required_button'] = "लगइन";
+
+$lang['error']['create_new_server'] = "आफ्नो सर्भर सिर्जना गर्नुहोस्";
+$lang['error']['create_new_server_description'] = "केही क्लिकहरूमा तपाईंले आफ्नो पहिलो सर्भर सिर्जना गर्न सक्नुहुन्छ।";
+$lang['error']['create_new_server_button'] = "सर्भर सिर्जना गर्नुहोस्";
+
+$lang['error']['construction'] = "यो पृष्ठ प्रगतिमा छ";
+$lang['error']['construction_description'] = "हामी हाल पृष्ठमा काम गरिरहेका छौं। यस बीचमा, बस्नुहोस् र तयारी गर्नुहोस्।";
+
+// सूचनाहरू
+$lang['notices']['premiumdiscount'] = "कुपन कोड प्राप्त गर्न <b>10% छुट</b> <b>6 महिनाको लागि</b> पावर र अल्टिमेटमा तपाईंको योजना अपग्रेड गर्दा प्रयोग गर्नुहोस् <b>UPGRADE10</b>।";
+$lang['notices']['premiaad'] = "हाम्रा फेलिक्स प्रिमियम योजनाहरू मार्फत 24/7 अनलाइन र विज्ञापन-रहित वेबसाइट जस्ता अतिरिक्त सुविधाहरू सहितको उत्कृष्ट हार्डवेयरमा आफ्नो सर्भर होस्ट गर्नुहोस्। <a href ='https://falixnodes.net/minecraft-server-hosting'>यहाँ क्लिक गर्नुहोस्</a>!";
+
+// widgets
+$lang['widgets']['panel_title'] = "तपाईँको प्यानल विवरण";
+$lang['widgets']['panel_ftp'] = "तपाईको FTP पासवर्ड";
+$lang['widgets']['panel_resetpass'] = "गलत पासवर्ड? तपाइँ <a href='/profile/settings'>प्रोफाइल सेटिङहरू</a> मा आफ्नो पासवर्ड रिसेट गर्न सक्नुहुन्छ।";
+$lang['widgets']['panel_loginbutton'] = "प्यानलमा लगइन गर्नुहोस्";
+$lang['widgets']['panel_closebutton'] = "बन्द";
+
+$lang['widgets']['changecpu_title'] = "CPU परिवर्तन गर्नुहोस्";
+$lang['widgets']['changecpu_newcpu'] = "सर्भरको नयाँ CPU कोर";
+$lang['widgets']['changecpu_changebutton'] = "CPU परिवर्तन गर्नुहोस्";
+$lang['widgets']['changecpu_closebutton'] = "बन्द";
+
+$lang['widgets']['changedisk_title'] = "डिस्क परिवर्तन गर्नुहोस्";
+$lang['widgets']['changedisk_newcpu'] = "सर्भरको नयाँ डिस्क स्पेस (एमबीमा)";
+$lang['widgets']['changedisk_changebutton'] = "डिस्क परिवर्तन गर्नुहोस्";
+$lang['widgets']['changedisk_closebutton'] = "बन्द";
+
+// quick links
+$lang['links']['quicklinks'] = "द्रुत लिङ्कहरू";
+$lang['links']['quicklinks_description'] = "हाम्रो द्रुत लिङ्कहरू ड्यासबोर्डमा चलिरहेकोसँग ";
+$lang['links']['createserver'] = "तपाईंको सर्भर सिर्जना गर्नुहोस्";
+$lang['links']['createserver_description'] = "आफ्नो Minecraft सर्भर सिर्जना गर्नुहोस्";
+$lang['links']['coinshop'] = "फेलिक्सकोइन्स स्टोरमा पसल";
+$lang['links']['coinshop_description'] = "तपाईंको Minecraft सर्भरको लागि बुस्टहरू किन्नुहोस्";
+$lang['links']['coins'] = "FelixCoins सित्तैमा कमाउनुहोस्";
+$lang['links']['coins_description'] = "हाम्रो भर्चुअल मुद्राको साथ तपाईंको सर्भरलाई बढावा दिनुहोस् ";
+$lang['links']['discordsupport'] = "डिस्कॉर्ड समर्थन";
+$lang['links']['discordsupport_description'] = "हाम्रो समुदायमा तपाइँका प्रश्नहरू सोध्नुहोस्";
+
+// resources widget
+$lang['resources']['serversbalance'] = "सर्भर ब्यालेन्स";
+$lang['resources']['rambalance'] = "RAM ब्यालेन्स";
+$lang['resources']['cpubalance'] = "CPU ब्यालेन्स";
+$lang['resources']['diskbalance'] = "डिस्क ब्यालेन्स";
+$lang['resources']['databasesbalance'] = "डेटाबेस ब्यालेन्स";
+$lang['resources']['portsbalance'] = "पोर्ट ब्यालेन्स";
+
+// login page
+$lang['auth']['login_title'] = "FalixNodes";
+$lang['auth']['login_description'] = "अहिले नै आफ्नो नि:शुल्क होस्टिङ प्राप्त गर्नुहोस्!";
+$lang['auth']['have_an_account'] = "खाता छ?";
+$lang['auth']['have_an_account_button'] = "लगइन";
+$lang['auth']['dont_have_account_yet'] = "खाता छैन? <br><a class='text-decoration-underline link-light' href='https://client.falixnodes.net /auth /register'>अहिले सिर्जना गर्नुहोस्!</a>";
+$lang['auth']['useragreement'] = "हाम्रो <a class='text-decoration-underline text-white' href='https://falixnodes.net/user-agreement'>प्रयोगकर्ता सम्झौता</a > पढ्नुहोस्";
+$lang['auth']['loginform_title'] = "खाता लगइन";
+$lang['auth']['form_email'] = "इमेल";
+$lang['auth']['form_password'] = "पासवर्ड";
+$lang['auth']['form_confirmpassword'] = "पासवर्ड पुष्टि गर्नुहोस्";
+$lang['auth']['loginform_forgotpassword'] = "पासवर्ड बिर्सनुभयो?";
+$lang['auth']['loginform_button'] = "लगइन";
+$lang['auth']['oauthlogins'] = "वा यी विकल्पहरूसँग लगइन गर्नुहोस्";
+$lang['auth']['register_oauthlogins'] = "वा यी विकल्पहरूसँग दर्ता गर्नुहोस्";
+$lang['auth']['google'] = "Google";
+$lang['auth']['discord'] = "डिस्कॉर्ड";
+$lang['auth']['registerform_title'] = "दर्ता गर्नुहोस्";
+$lang['auth']['registerform_button'] = "दर्ता गर्नुहोस्";
+$lang['auth']['registerform_tosaccept'] = "म <a href='https://falixnodes.net/user-agreement'>प्रयोगकर्ता सम्झौता </a> र <a href='https:// /falixnodes .net/privacy-policy'>गोपनीयता नीति</a> म स्वीकार गर्छु";
+$lang['auth']['registerform_marketingconsent'] = "यदि तपाइँ हाम्रो नयाँ प्रस्तावहरु संग मार्केटिङ मेल प्राप्त गर्न चाहनुहुन्छ भने यो बाकस जाँच गर्नुहोस्।";
+
+// index.php / main page
+$lang['manage_servers'] = "हाम्रो प्रयोगकर्ता मैत्री प्यानलको साथ आफ्नो सर्भरहरू नियन्त्रण गर्नुहोस्।";
+
+// सर्भर तालिका
+$lang ['servers'] = "तपाईंको सर्भर";
+$lang['logintogamepanel'] = "खेल प्यानलमा लगइन गर्नुहोस्";
+$lang['logintogamepanel_button'] = "खेल प्यानल";
+
+$lang['server']['name'] = "सर्भर नाम";
+$lang['server']['cpu'] = "CPU";
+$lang['server']['ram'] = "RAM";
+$lang['server']['disk'] = "DISK";
+$lang['server']['database'] = "डेटाबेस";
+$lang['server']['port'] = "पोर्ट";
+
+$lang['server']['gotoserver'] = "सर्भरमा जानुहोस्";
+$lang['server']['changecpu'] = "CPU परिवर्तन गर्नुहोस्";
+$lang['server']['changeram'] = "RAM परिवर्तन गर्नुहोस्";
+$lang['server']['changedisk'] = "डिस्क परिवर्तन गर्नुहोस्";
+$lang['server']['deleteserver'] = "सर्भर मेटाउनुहोस्";
+
+// create server page
+$lang['create']['createserver'] = "सर्भर सिर्जना गर्नुहोस्";
+$lang ['create']['servername'] = "सर्भरनाम";
+$lang['create']['serverram'] = "सर्भर RAM";
+$lang['create']['servergame'] = "खेल";
+$lang['create']['allgames'] = "सरे उपलब्ध खेलहरू";
+$lang['create']['selectgame'] = "खेल चयन गर्नुहोस्";
+$lang['create']['minecrafteula'] = "म <a href='https://account.mojang.com/documents/minecraft_eula/'>Minecraft End User License Agreement (EULA)</a> स्वीकार गर्छु हुह";
+$lang['create']['createbuton'] = "सर्भर सिर्जना गर्नुहोस्";
+$lang['create']['adblock'] = "(((यदि बटनले 3-4 सेकेन्ड पछि काम गर्दैन भने, आफ्नो adblock बन्द गर्नुहोस्। )))";
+
+// change ram page
+$lang['changeram']['title'] = "RAM परिवर्तन गर्नुहोस्";
+$lang ['changeram']['servername'] = "सर्भरनाम";
+$lang['changeram']['currentram'] = "हालको RAM";
+$lang['changeram']['newram'] = "नयाँ RAM";
+$lang['changeram']['changerambutton'] = "RAM परिवर्तन गर्नुहोस्";
+
+// सिक्का पृष्ठ
+$lang['coins']['earnfalixcoins'] = "FalixCoins कमाउनुहोस्";
+$lang['coins']['resourcestore'] = "FalixCoins पसल";
+$lang['coins']['task'] = "कार्य";
+$lang['coins']['reward'] = "पुरस्कार";
+$lang['coins']['earn'] = "कमाउ";
+$lang['coins']['claim'] = "पुरस्कार लिनुहोस्";
+$lang['coins']['invitefriends'] = "आफ्ना साथीहरूलाई आमन्त्रित गर्नुहोस्";
+$lang['coins']['joinforrewards'] = "पुरस्कारको लागि सामेल हुनुहोस्";
+$lang['coins']['invitereward'] = "एउटा निमन्त्रणाको लागि 1,000 FelixCoins प्राप्त गर्नुहोस्";
+$lang['coins']['joinreward'] = "सर्भरमा सामेल हुन ५० FelixCoins सम्म प्राप्त गर्नुहोस्";
+
+// affiliate page
+$lang['affiliate']['title'] = "मित्रलाई आमन्त्रित गर्न 1,000 FelixCoins प्राप्त गर्नुहोस्";
+$lang['affiliate']['description'] = "आफ्नो साथीलाई आमन्त्रित गर्नुहोस् र सेकेन्डमा पुरस्कृत गर्नुहोस्।";
+$lang['affiliate']['seconddescription'] = "आफ्ना साथीहरूलाई आमन्त्रित गर्न, तलको वेबसाइट लिङ्क प्रतिलिपि गर्नुहोस् र आफ्ना साथीहरूलाई पठाउनुहोस्।";
+$lang['affiliate']['footer'] = "जब तपाइँ साथीहरूलाई आमन्त्रित गर्नुहुन्छ, तपाइँ तपाइँको <a href='https://client.falixnodes.net/affiliate/status'>रेफरलहरू</a को स्थिति हेर्न सक्नुहुन्छ। > र यदि तपाइँसँग कुनै प्रश्नहरू छन् भने, तपाइँ हामीलाई Discord सर्भरमा सम्पर्क गर्न सक्नुहुन्छ।";
+
+// activity page
+$lang['activity']['title'] = "गतिविधि";
+$lang['activity']['id'] = "ID";
+$lang['activity']['action'] = "घटना";
+$lang['activity']['description'] = "विवरण";
+$lang['activity']['useragent'] = "USER-AGENT";
+$lang['activity']['ip'] = "IP ADDRESS";
+$lang['activity']['date'] = "मिति";
+
+// प्रोफाइल पृष्ठ
+$lang['profile']['settings_title'] = "प्रोफाइल सेटिङहरू ";
+$lang['profile']['firstname'] = "पहिलो नाम";
+$lang['profile']['lastname'] = "अन्तिम नाम";
+$lang['profile']['email'] = "इमेल";
+$lang['profile']['theme'] = "वेबसाइट विषयवस्तु";
+$lang['profile']['textcolor'] = "पाठको रंग";
+
+$lang['profile']['servversettings_title'] = "सर्भर सेटिङहरू";
+$lang['profile']['externalserverstart'] = "बाह्य सर्भर सुरु";
+$lang['profile']['update'] = "अपडेट";
+$lang['profile']['enable'] = "सक्षम गर्नुहोस्";
+$lang['profile']['disable'] = "असक्षम";
+
+$lang['profile']['emailpreference_title'] = "इमेल प्राथमिकताहरू";
+$lang['profile']['emailpreference'] = "आफ्नो इमेल प्राथमिकता परिवर्तन गर्नुहोस्";
+
+$lang['profile']['changepassword_title'] = "पासवर्ड परिवर्तन गर्नुहोस्";
+$lang['profile']['changepassword_notice'] = "NB! यदि तपाइँ खेल प्यानल पासवर्ड परिवर्तन गर्न प्रयास गर्दै हुनुहुन्छ भने, कृपया अर्को पासवर्ड स्तम्भको लागि पृष्ठ अप गर्नुहोस्।";
+$lang['profile']['oldpassword'] = "पुरानो पासवर्ड";
+$lang['profile']['newpassword'] = "नयाँ पासवर्ड";
+$lang['profile']['confirmpassword'] = "पासवर्ड पुष्टि गर्नुहोस्";
+$lang['profile']['updatepassword'] = "पासवर्ड अपडेट गर्नुहोस्";
+
+$lang['profile']['discord'] = "डिस्कॉर्ड";
+$lang['profile']['authorised'] = "को रूपमा अधिकृत";
+$lang['profile']['linkdiscord'] = "लिंक डिस्कोर्ड";
+$lang['profile']['unlinkdiscord'] = "अनलिंक Discord";
+$lang['profle']['notlinked'] = "तपाईँको खाता Discord खातामा लिङ्क गरिएको छैन। यसलाई तपाइँको Discord खाता लिङ्क गर्न अधिकार दिनुहोस्।";
+
+$lang['profile']['dangerzone'] = "खतरा क्षेत्र";
+$lang['profile']['changegamepanelpassword_title'] = "खेल प्यानलको पासवर्ड परिवर्तन गर्नुहोस्";
+$lang['profile']['changegamepanelpassword_description'] = "पासवर्ड परिवर्तन भएपछि पुरानो पासवर्डले काम गर्दैन।";
+$lang['profile']['changegamepanelpassword_button'] = "पासवर्ड परिवर्तन गर्नुहोस्";
+$lang['profile']['deleteaccount_title'] = "खाता मेट्नुहोस्";
+$lang['profile']['deleteaccount_description'] = "एकपटक तपाईंले आफ्नो खाता मेटाएपछि फिर्ता हुने छैन। कृपया निश्चित हुनुहोस्।";
+$lang['profile']['deleteaccount_button'] = "खाता मेट्नुहोस्";
+
+// falixcoins store
+$lang['store']['plan_expire_on'] = "तपाईंको योजना यस मितिमा रिसेट हुनेछ";
+$lang['store']['month'] = "महिना";
+$lang['store']['cancelplan'] = "योजना रद्द गर्नुहोस्";
+$lang['store']['purchase'] = "अहिले किन्नुहोस्";
+$lang['store']['booster'] = "बूस्टर";
+
+$lang['store']['booster1_servers'] = "3 सर्भर सम्म सिर्जना गर्नुहोस्";
+$lang['store']['booster1_cpu'] = "प्रति सर्भर १२०% CPU पावर प्राप्त गर्नुहोस्";
+$lang['store']['booster1_ram'] = "6GB RAM प्राप्त गर्नुहोस्";
+$lang['store']['booster1_disk'] = "प्रति सर्भर 15GB डिस्क प्राप्त गर्नुहोस्";
+$lang['store']['booster1_mysql'] = "प्रति सर्भर ३ MySQL डाटाबेसहरू प्राप्त गर्नुहोस्";
+$lang['store']['booster1_ports'] = "प्रति सर्भर २ अतिरिक्त पोर्टहरू प्राप्त गर्नुहोस्";
+
+$lang['store']['booster2_servers'] = "4 सर्भर सम्म सिर्जना गर्नुहोस्";
+$lang['store']['booster2_cpu'] = "प्रति सर्भर 130% CPU पावर प्राप्त गर्नुहोस्";
+$lang['store']['booster2_ram'] = "७ जीबी मेमोरी प्राप्त गर्नुहोस्";
+$lang['store']['booster2_disk'] = "प्रति सर्भर १५ जीबी डिस्क प्राप्त गर्नुहोस्";
+$lang['store']['booster2_mysql'] = "प्रति सर्भर 3 MySQL डाटाबेसहरू प्राप्त गर्नुहोस्";
+$lang['store']['booster2_ports'] = "प्रति सर्भर २ अतिरिक्त पोर्टहरू प्राप्त गर्नुहोस्";
+
+$lang['store']['booster3_servers'] = "5 सर्भर सम्म सिर्जना गर्नुहोस्";
+$lang['store']['booster3_cpu'] = "प्रति सर्भर 140% CPU पावर प्राप्त गर्नुहोस्";
+$lang['store']['booster3_ram'] = "८ जीबी मेमोरी प्राप्त गर्नुहोस्";
+$lang['store']['booster3_disk'] = "प्रति सर्भर १५ जीबी डिस्क प्राप्त गर्नुहोस्";
+$lang['store']['booster3_mysql'] = "प्रति सर्भर 3 MySQL डाटाबेसहरू प्राप्त गर्नुहोस्";
+$lang['store']['booster3_ports'] = "प्रति सर्भर २ अतिरिक्त पोर्टहरू प्राप्त गर्नुहोस्";
+
+// tutorials
+$lang['tutorials']['createserver_stepone_title'] = "तपाईंको सर्भर सिर्जना गर्नुहोस्";
+$lang['tutorials']['createserver_stepone'] = "पहिलो पटक यहाँ? चिन्ता नगर्नुहोस्!<br />
+आफ्नो पहिलो सर्भरको साथ फेलिक्स प्रयोग गर्न सुरु गर्नुहोस्। <a href='/create' target='_blank'>सर्भर पृष्ठ सिर्जना गर्नुहोस्</a> मा जानुहोस् र फारम भरेर सर्भर सिर्जना गर्न सुरु गर्नुहोस्।<br />
+<br />
+<b>सर्भर नाम:</b> 'Friends Server' वा 'My Server' जस्तै सर्भरको नाम दिनुहोस्।<br />
+<b>सर्भर RAM:</b>यो थोरै गाह्रो हुन सक्छ तर चिन्ता नगर्नुहोस्! तपाईंले हाम्रो व्याख्या पढ्न सक्नुहुन्छ <a data-bs-toggle='offcanvas' data-bs-target='#RAMExplanation' aria-controls='RAMExplanation' style='text-decoration: underline dotted; कर्सर: सूचक; href='#'>यहाँ क्लिक गर्नुहोस्</a> पढ्नको लागि।<br />
+<br />
+फारम भरिसकेपछि 'Create Server' मा क्लिक गर्नुहोस् र... बधाई छ! तपाईको पहिलो सर्भर बनाइएको छ। सर्भर कसरी पहुँच गर्ने भनेर जान्नको लागि 'सर्भर व्यवस्थापन' खण्डमा जानुहोस्।";
+
+$lang['tutorials']['createserver_steptwo_title'] = "सर्भर सेटअप गर्दै";
+$lang['tutorials']['createserver_steptwo'] = "तपाईंले आफ्नो पहिलो सर्भर सिर्जना गर्नुभयो। अब के हुन्छ?<br />
+त्यो जस्तै सरल, तपाइँ सर्भर तालिकाको दाहिने कुनामा रहेको 'खेल प्यानलमा लगइन गर्नुहोस्' बटनमा क्लिक गरेर आफ्नो गेम प्यानल खातामा लगइन गर्न सक्नुहुन्छ।<br />
+<br />
+यस पछि तपाईले खेल प्यानल पृष्ठ देख्नुहुनेछ। पृष्ठमा आफ्नो सर्भर फेला पार्नुहोस् र यसमा क्लिक गर्नुहोस्। यसपछि भर्सन ट्याबमा जानुहोस् जुन नेभिगेसन बारमा फेला पर्नेछ।<br />
+<br />
+त्यसपछि तपाइँलाई तपाइँको सर्भरको लागि सबैभन्दा उपयुक्त सफ्टवेयर र संस्करण छनोट गर्न सोधिनेछ। हामी तपाइँलाई कागजबाट सुरु गर्न सिफारिस गर्दछौं किनकि यो सबै संस्करणहरूको लागि उपलब्ध सबैभन्दा अनुकूलित सफ्टवेयर हो। त्यस पछि कृपया आफ्नो संस्करण खोज्नुहोस् र यसलाई चयन गर्नुहोस्।<br />
+<br />
+एकचोटि तपाईंले यो सबै गरेपछि, तपाईंको सर्भर स्थापना हुन्छ। अब सर्भर कन्सोलमा जानुहोस् र सर्भर सुरु गर्न 'स्टार्ट' बटनमा क्लिक गर्नुहोस्।<br />
+<br />
+राम्रो भयो, तपाईंको सर्भर चलिरहेको छ! यसलाई कसरी जडान गर्ने जान्नको लागि अर्को ट्यूटोरियलमा जानुहोस्।";
+
+$lang['tutorials']['createserver_stepthree_title'] = "तपाईंको सर्भरमा जडान गर्नुहोस्";
+$lang['tutorials']['createserver_stepthree'] = "तपाईँको सर्भर अहिले चलिरहेको छ। अब के हुन्छ?<br />
+यो खेल्ने समय हो तर कसरी?<br />
+<br />
+तपाईंले खेल प्यानलमा चयन गर्नुभएको संस्करणको साथ तपाईंको Minecraft ग्राहक सुरु गर्नुहोस्। सकियो? ठीक छ।<br />
+अब तपाईंले आफ्नो सर्भर अद्वितीय होस्टनाम र यसको पोर्ट प्राप्त गर्नुपर्छ, यो कन्सोल पृष्ठको तल तिर फेला पार्न सकिन्छ। यो सामान्यतया <b>game#.falixserver.net:00000</b> बाट सुरु हुन्छ। फेला पर्‍यो? Minecraft Multiplayer मा प्रतिलिपि गरेर टाँस्नुहोस्।<br />
+<br />
+यसमा जडान गर्नुहोस् र... उत्कृष्ट, तपाईं सर्भर भित्र हुनुहुन्छ! आफ्नो साथीहरु संग रमाइलो! यो एकदम सरल थियो।";
