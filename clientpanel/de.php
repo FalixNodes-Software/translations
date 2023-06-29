6b859b91c5663aed2afb254dc5b9c298e9a365eb
Lovinor <104237836+Lovinor@users.noreply.github.com>
2023-06-29 22:41:34 +0200

    Update de.php
    nicht fertig

--- clientpanel/de.php
+++ clientpanel/de.php
@@ -1,88 +1,269 @@
 <?php
 
+// page titles
+$lang['title']['mainpage_title'] = "Verwaltung";
+$lang['title']['mainpage_description'] = "Verwalten sie ihre Minecraft Server.";
+
+$lang['title']['createserver_title'] = "Server erstellen";
+$lang['title']['createserver_description'] = "Erstelle deinen eigenen Minecraft Server.";
+
+$lang['title']['changeram_title'] = "Arbeitsspeicher";
+$lang['title']['changeram_description'] = "Verwalten sie ihre Server-Resourcen.";
+
+$lang['title']['alwaysonline_title'] = "Immer Online";
+$lang['title']['alwaysonline_description'] = "Noch nicht verfügbar.";
+
+$lang['title']['coins_title'] = "Coins";
+$lang['title']['coins_description'] = "Erhalte Coins und tausche sie gegen Minecraft Server-Boosts ein.";
+
 // navigation bar
 $lang['nav']['dashboard'] = "Dashboard";
 $lang['nav']['coins'] = "Coins";
 $lang['nav']['mainpage'] = "Hauptseite";
 $lang['nav']['servers'] = "Server";
 $lang['nav']['createserver'] = "Server erstellen";
-$lang['nav']['changeram'] = "RAM ändern";
-$lang['nav']['alwaysonline'] = "Immer online";
+$lang['nav']['changeram'] = "Arbeitsspeicher";
+$lang['nav']['alwaysonline'] = "Immer Online";
 $lang['nav']['soon'] = "Bald";
-$lang['nav']['account'] = "Konto";
-$lang['nav']['logintopanel'] = "Anmeldung im Server Interface";
+$lang['nav']['account'] = "Account";
+$lang['nav']['logintopanel'] = "Login ins Panel";
 $lang['nav']['plandetails'] = "Plan Details";
-$lang['nav']['billing'] = "Rechnungsstellung";
+$lang['nav']['billing'] = "Billing";
+
+// errors
+$lang['error']['login_is_required'] = "Eine Anmeldung ist Erforderlich!";
+$lang['error']['login_is_required_description'] = "Loggen Sie sich ein, um Ihre Server zu sehen.";
+$lang['error']['login_is_required_button'] = "Login";
+
+$lang['error']['create_new_server'] = "Erstellen Sie Ihren eigenen Server!";
+$lang['error']['create_new_server_description'] = "Sie können Ihren ersten Server mit nur wenigen Klicks erstellen.";
+$lang['error']['create_new_server_button'] = "Server erstellen";
+
+$lang['error']['construction'] = "Diese Seite befindet sich derzeit im Aufbau!";
+$lang['error']['construction_description'] = "Wir arbeiten derzeit daran, in der Zwischenzeit können Sie sich darauf vorbereiten.";
 
 // notices
-$lang['notices']['premiumdiscount'] = "Erhalten Sie bis zu <b>10 % Rabatt</b> bis zu <b>6 Monaten</b> mit dem Rabattcode <b>UPGRADE10</b>, wenn Sie Ihren Tarif auf Power oder Ultimate upgraden wollen.";
-$lang['notices']['premiumad'] = "Hosten Sie Ihren eigenen Server auf erstklassiger Hardware mit zusätzlichen Funktionen wie 24/7 Uptime und einer werbefreien Webseite mit unseren Premium Tarifen zu Preisen unter dem Marktniveau. <a href='https://falixnodes.net/minecraft-server-hosting'>Klick mich</a>!";
+$lang['notices']['premiumdiscount'] = "Bekomm ein <b>10% Discount</b> für <b>6 Monates</b> mit dem Gutscheincode <b>UPGRADE10</b> wenn Sie Ihren derzeitigen Tarif auf Power oder Ultimate aufrüsten.";
+$lang['notices']['premiumad'] = "Hosten Sie Ihren Server auf erstklassiger Hardware mit zusätzlichen Funktionen wie 24/7 Uptime und ein werbefreies Interface mit unseren Falix Premium Plänen zu Preisen unter dem Marktniveau. <a href='https://falixnodes.net/minecraft-server-hosting'>Klick auf mich</a>!";
 
+// widgets
+$lang['widgets']['panel_title'] = "Details zu Ihrem Panel";
+$lang['widgets']['panel_ftp'] = "Ihr FTP Passwort";
+$lang['widgets']['panel_resetpass'] = "Falsches Passwort entdeckt! Sie können Ihr Passwort zurücksetzen unter ihren <a href='/profile/settings'>Profil Einstellungen</a>.";
+$lang['widgets']['panel_loginbutton'] = "Anmeldung im Panel";
+$lang['widgets']['panel_closebutton'] = "Schließen";
+
+$lang['widgets']['changecpu_title'] = "CPU Anzahl";
+$lang['widgets']['changecpu_newcpu'] = "Neue CPU Kerne hinzufügen";
+$lang['widgets']['changecpu_changebutton'] = "CPU Anzahl ändern";
+$lang['widgets']['changecpu_closebutton'] = "Schließen";
+
+$lang['widgets']['changedisk_title'] = "Speicherplatz";
+$lang['widgets']['changedisk_newcpu'] = "Neuer Server-Speicherplatz (in Megabyte)";
+$lang['widgets']['changedisk_changebutton'] = "Speicherplatz ändern";
+$lang['widgets']['changedisk_closebutton'] = "Schließen";
 
 // quick links
-$lang['links']['quicklinks'] = "Schnell-Links";
-$lang['links']['quicklinks_description'] = "Navigieren Sie mit unseren Schnell-Links durch das Dashboard";
+$lang['links']['quicklinks'] = "Quick Links";
+$lang['links']['quicklinks_description'] = "Navigieren Sie mit unseren Quick Links durch das Dashboard";
 $lang['links']['createserver'] = "Erstellen Sie Ihren eigenen Server";
-$lang['links']['createserver_description'] = "Erstellen Sie Ihren eigenen Minecraft-Server";
-$lang['links']['coinshop'] = "Einkaufen im FalixCoins Shop";
-$lang['links']['coinshop_description'] = "Kaufen Sie Erweiterungen für Ihren Minecraft-Server";
-$lang['links']['coins'] = "FalixCoins kostenlos verdienen";
-$lang['links']['coins_description'] = "Steigern Sie Ihren Server, indem Sie unsere virtuelle Währung verdienen";
-$lang['links']['discordsupport'] = "Discord-Support";
-$lang['links']['discordsupport_description'] = "Stellen Sie Ihre Fragen an unserer Community!";
+$lang['links']['createserver_description'] = "Erstelle deinen eigenen Minecraft server";
+$lang['links']['coinshop'] = "Einkaufen im FalixCoins Store";
+$lang['links']['coinshop_description'] = "Kaufen Sie Boosts für Ihren Minecraft Server";
+$lang['links']['coins'] = "Verdienen Sie FalixCoins kostenlos";
+$lang['links']['coins_description'] = "Verbessern Sie Ihren Server, indem Sie unsere virtuelle Währung verdienen";
+$lang['links']['discordsupport'] = "Discord Support";
+$lang['links']['discordsupport_description'] = "Stellen Sie Ihre Fragen an unsere Community!";
 
 // resources widget
-$lang['resources']['serversbalance'] = "Server Anzahl";
+$lang['resources']['serversbalance'] = "Servers Anzahl";
 $lang['resources']['rambalance'] = "Arbeitsspeicher Anzahl";
 $lang['resources']['cpubalance'] = "CPU Anzahl";
 $lang['resources']['diskbalance'] = "Speicherplatz Anzahl";
 $lang['resources']['databasesbalance'] = "Datenbanken Anzahl";
 $lang['resources']['portsbalance'] = "Port Anzahl";
 
+// login page
+$lang['auth']['login_title'] = "FalixNodes";
+$lang['auth']['login_description'] = "Holen Sie sich jetzt Ihr kostenloses Server Hosting!";
+$lang['auth']['have_an_account'] = "Haben Sie bereits ein Konto?";
+$lang['auth']['have_an_account_button'] = "Login";
+$lang['auth']['dont_have_account_yet'] = "Haben Sie noch kein Konto? <br><a class='text-decoration-underline link-light' href='https://client.falixnodes.net/auth/register'>Los geht's!</a>";
+$lang['auth']['useragreement'] = "Lesen Sie unsere <a class='text-decoration-underline text-white' href='https://falixnodes.net/user-agreement'>Benutzervereinbarung</a>";
+$lang['auth']['loginform_title'] = "Account Login";
+$lang['auth']['form_email'] = "E-Mail-Adresse";
+$lang['auth']['form_password'] = "Passwort";
+$lang['auth']['form_confirmpassword'] = "Passwort Bestätigen";
+$lang['auth']['loginform_forgotpassword'] = "Passwort Vergessen?";
+$lang['auth']['loginform_button'] = "Login";
+$lang['auth']['oauthlogins'] = "oder loggen Sie sich ein mit";
+$lang['auth']['register_oauthlogins'] = "oder registrieren Sie sich mit";
+$lang['auth']['google'] = "Google";
+$lang['auth']['discord'] = "Discord";
+$lang['auth']['registerform_title'] = "Registrieren";
+$lang['auth']['registerform_button'] = "Registrieren";
+$lang['auth']['registerform_tosaccept'] = "Ich akzeptiere die <a href='https://falixnodes.net/user-agreement'>Benutzervereinbarung </a>und <a href='https://falixnodes.net/privacy-policy'>Datenschutzrichtlinien</a> zu.";
+$lang['auth']['registerform_marketingconsent'] = "Markieren Sie dieses Feld, wenn Sie Marketing-E-Mails mit unseren neuesten Angeboten erhalten möchten.";
+
 // index.php / main page
-$lang['manage_servers'] = "Steuern Sie Ihre Server mit unserem benutzerfreundlichen Server Interface";
+$lang['manage_servers'] = "Verwalten Sie Ihre Server mit unserem benutzerfreundlichen Server Panel.";
 
 // servers table
-$lang['servers'] = "Ihre Server";
-$lang['logintogamepanel'] = "Anmeldung im Server Interface";
+$lang['servers'] = "Deine Server";
+$lang['logintogamepanel'] = "Login ins Server Panel";
+$lang['logintogamepanel_button'] = "Server Panel";
 
-$lang['server']['name'] = "SERVER-NAME";
+$lang['server']['name'] = "SERVER NAME";
 $lang['server']['cpu'] = "CPU";
 $lang['server']['ram'] = "ARBEITSSPEICHER";
 $lang['server']['disk'] = "SPEICHERPLATZ";
 $lang['server']['database'] = "DATENBANK";
-$lang['server']['port'] = "PORT";
+$lang['server']['port'] = "PORTS";
 
-$lang['server']['gotoserver'] = "Zum Server navigieren";
+$lang['server']['gotoserver'] = "Zum Server";
 $lang['server']['changecpu'] = "CPU ändern";
 $lang['server']['changeram'] = "Arbeitsspeicher ändern";
-$lang['server']['changedisk'] = "Speicherplatz ändern";
-$lang['server']['deleteserver'] = "Server löschen";
+$lang['server']['changedisk'] = "Speicherplatz";
+$lang['server']['deleteserver'] = "Server Löschen";
 
 // create server page
-$lang['create']['createserver'] = "Server erstellen";
-$lang['create']['servername'] = "Name des Servers";
-$lang['create']['serverram'] = "Arbeitsspeicher des Servers";
+$lang['create']['createserver'] = "Server Erstellen";
+$lang['create']['servername'] = "Server Name";
+$lang['create']['serverram'] = "Arbeitsspeicher";
 $lang['create']['servergame'] = "Spiel";
 $lang['create']['allgames'] = "Alle unterstützten Spiele";
-$lang['create']['selectgame'] = "Wählen Sie ihr Spiel";
-$lang['create']['minecrafteula'] = "Ich akzeptiere Sie den <a href='https://account.mojang.com/documents/minecraft_eula/'>Minecraft Endbenutzer-Lizenzvertrag (EULA)</a>";
-$lang['create']['createbutton'] = "Erstellen.";
+$lang['create']['selectgame'] = "Spiel";
+$lang['create']['minecrafteula'] = "I accept the <a href='https://account.mojang.com/documents/minecraft_eula/'>Minecraft End User License Agreement (EULA)</a>";
+$lang['create']['createbutton'] = "Create";
+$lang['create']['adblock'] = "((( If button do not work after 3-5 seconds of waiting, please disable your adblock. )))";
+
+// change ram page
+$lang['changeram']['title'] = "Change RAM";
+$lang['changeram']['servername'] = "Server Name";
+$lang['changeram']['currentram'] = "Current RAM";
+$lang['changeram']['newram'] = "New RAM";
+$lang['changeram']['changerambutton'] = "Change RAM";
 
 // coins page
-$lang['coins']['earnfalixcoins'] = "FalixCoins verdienen";
+$lang['coins']['earnfalixcoins'] = "Earn FalixCoins";
 $lang['coins']['resourcestore'] = "FalixCoins Shop";
-$lang['coins']['task'] = "Aufgabe";
-$lang['coins']['reward'] = "Belohnung";
-$lang['coins']['earn'] = "Verdienen Sie";
-$lang['coins']['claim'] = "Beanspruchen";
-$lang['coins']['invitefriends'] = "Laden Sie Ihre Freunde ein";
-$lang['coins']['joinforrewards'] = "Beitreten für Prämien";
-$lang['coins']['invitereward'] = "1.000 FalixCoins pro eingeladenem Freund";
-$lang['coins']['joinreward'] = "bis zu 50 FalixCoins pro Server";
+$lang['coins']['task'] = "Task";
+$lang['coins']['reward'] = "Reward";
+$lang['coins']['earn'] = "Earn";
+$lang['coins']['claim'] = "Claim";
+$lang['coins']['invitefriends'] = "Invite your friends";
+$lang['coins']['joinforrewards'] = "Join for rewards";
+$lang['coins']['invitereward'] = "1,000 FalixCoins per invited friend";
+$lang['coins']['joinreward'] = "up to 50 FalixCoins per joined server";
 
 // affiliate page
-$lang['affiliate']['title'] = "Laden Sie einen Freund ein und erhalten Sie 1.000 FalixCoins";
-$lang['affiliate']['description'] = "Laden Sie Ihre Freunde ein und erhalten Sie Ihre Belohnung in nur Sekundenschnelle!";
-$lang['affiliate']['seconddescription'] = "Laden Sie Ihre Freunde ein, indem Sie dem unten stehenden Link zur Webseite kopieren und an Ihre Freunde senden.";
-$lang['affiliate']['footer'] = "Sobald Sie Freunde eingeladen haben, können Sie <a href='https://client.falixnodes.net/affiliate/status'>den Status Ihrer Überweisungen überwachen</a> oder kontaktieren sie uns auf unserem Discord-Server, wenn Sie Fragen haben.";
+$lang['affiliate']['title'] = "Invite a friend and get 1,000 FalixCoins";
+$lang['affiliate']['description'] = "Invite your friends and get your reward in seconds.";
+$lang['affiliate']['seconddescription'] = "Invite your friends by copying website link from below and send it to your friends.";
+$lang['affiliate']['footer'] = "Once you've invited friends, you can <a href='https://client.falixnodes.net/affiliate/status'>view the status of your referrals</a> or contact us at our Discord server if you have any questions.";
+
+// activity page
+$lang['activity']['title'] = "Activity";
+$lang['activity']['id'] = "ID";
+$lang['activity']['action'] = "ACTION";
+$lang['activity']['description'] = "DESCRIPTION";
+$lang['activity']['useragent'] = "USER-AGENT";
+$lang['activity']['ip'] = "IP ADDRESS";
+$lang['activity']['date'] = "DATE";
+
+// profile page
+$lang['profile']['settings_title'] = "Profile Settings";
+$lang['profile']['firstname'] = "First Name";
+$lang['profile']['lastname'] = "Last Name";
+$lang['profile']['email'] = "Email";
+$lang['profile']['theme'] = "Website Theme";
+$lang['profile']['textcolor'] = "Text Color";
+
+$lang['profile']['serversettings_title'] = "Server Settings";
+$lang['profile']['externalserverstart'] = "External Server Start";
+$lang['profile']['update'] = "Update";
+$lang['profile']['enable'] = "Enable";
+$lang['profile']['disable'] = "Disable";
+
+$lang['profile']['emailpreference_title'] = "Email Preference";
+$lang['profile']['emailpreference'] = "Change your email preference";
+
+$lang['profile']['changepassword_title'] = "Change Password";
+$lang['profile']['changepassword_notice'] = "NB! If you are trying to change game panel password, scroll down to the other password prompt.";
+$lang['profile']['oldpassword'] = "Old Password";
+$lang['profile']['newpassword'] = "New Password";
+$lang['profile']['confirmpassword'] = "Confirm Password";
+$lang['profile']['updatepassword'] = "Update Password";
+
+$lang['profile']['discord'] = "Discord";
+$lang['profile']['authorised'] = "Authorised as";
+$lang['profile']['linkdiscord'] = "Link Discord";
+$lang['profile']['unlinkdiscord'] = "Unlink Discord";
+$lang['profle']['notlinked'] = "You do not have Discord account linked to the account, to link your Discord account, authorise your Discord account";
+
+$lang['profile']['dangerzone'] = "Danger Zone";
+$lang['profile']['changegamepanelpassword_title'] = "Change Game Panel's password";
+$lang['profile']['changegamepanelpassword_description'] = "Once you change the Game Panel password, old password will no longer work.";
+$lang['profile']['changegamepanelpassword_button'] = "Change Password";
+$lang['profile']['deleteaccount_title'] = "Delete this account";
+$lang['profile']['deleteaccount_description'] = "Once you delete a account, there is no going back. Please be certain.";
+$lang['profile']['deleteaccount_button'] = "Delete Account";
+
+// falixcoins store
+$lang['store']['plan_expire_on'] = "Your plan will auto renew at";
+$lang['store']['month'] = "mo";
+$lang['store']['cancelplan'] = "Cancel Plan";
+$lang['store']['purchase'] = "Purchase Now";
+$lang['store']['booster'] = "Booster";
+
+$lang['store']['booster1_servers'] = "Create up to 3 servers";
+$lang['store']['booster1_cpu'] = "Get 120% CPU Power/each server";
+$lang['store']['booster1_ram'] = "Get 6 GB memory";
+$lang['store']['booster1_disk'] = "Get 15 GB disk/each server";
+$lang['store']['booster1_mysql'] = "Get 3 MySQL databases/each server";
+$lang['store']['booster1_ports'] = "Get 2 Extra Ports/each server";
+
+$lang['store']['booster2_servers'] = "Create up to 4 servers";
+$lang['store']['booster2_cpu'] = "Get 130% CPU Power/each server";
+$lang['store']['booster2_ram'] = "Get 7 GB memory";
+$lang['store']['booster2_disk'] = "Get 15 GB disk/each server";
+$lang['store']['booster2_mysql'] = "Get 3 MySQL databases/each server";
+$lang['store']['booster2_ports'] = "Get 2 Extra Ports/each server";
+
+$lang['store']['booster3_servers'] = "Create up to 5 servers";
+$lang['store']['booster3_cpu'] = "Get 140% CPU Power/each server";
+$lang['store']['booster3_ram'] = "Get 8 GB memory";
+$lang['store']['booster3_disk'] = "Get 15 GB disk/each server";
+$lang['store']['booster3_mysql'] = "Get 3 MySQL databases/each server";
+$lang['store']['booster3_ports'] = "Get 2 Extra Ports/each server";
+
+// tutorials
+$lang['tutorials']['createserver_stepone_title'] = "Create your server";
+$lang['tutorials']['createserver_stepone'] = "Your first time being here? No worries!<br />
+Start using Falix by creating your first server. Navigate to <a href='/create' target='_blank'>Create Server page</a> and start the creation process by filling out the form.<br />
+<br />
+<b>Server Name:</b> Name your server like 'Friends Server' or 'My Server'.<br />
+<b>Server RAM:</b> This might be bit confusing, but don't worry! Read our explanation by <a data-bs-toggle='offcanvas' data-bs-target='#RAMExplanation' aria-controls='RAMExplanation' style='text-decoration: underline dotted; cursor: pointer;' href='#'>clicking here</a>.<br />
+<br />
+After you have filled the form, click 'Create' and... congratulations! Your first own server has been created. Navigate to 'Manage your server' to find out how to access it.";
+
+$lang['tutorials']['createserver_steptwo_title'] = "Manage your server";
+$lang['tutorials']['createserver_steptwo'] = "You have created your first server, what next?<br />
+Simple enough, you login to your game panel account by clicking the button 'Login to Game Panel' in the right corner of the servers table.<br />
+<br />
+After it, you will see game panel interface. Find your server in the interface and click on it. After it, make sure to navigate to Versions tab which can be found in the navigation bar.<br />
+<br />
+You will be then asked to select most appropriate software and version for your server. We recommend you to start with Paper since it is most optimized software available for all versions. Then please find your version and select it.<br />
+<br />
+Once you have done that, your server has been installed. Now navigate to your server console and start the server by clicking the button 'Start'.<br />
+<br />
+Awesome, your server is now running! Head to next tutorial to figure out how to connect to it.";
+
+$lang['tutorials']['createserver_stepthree_title'] = "Connect to your server";
+$lang['tutorials']['createserver_stepthree'] = "Your server is running, what next?<br />
+Time to play on it! But how?<br />
+<br />
+Start your Minecraft client with the version you have selected in the game panel. Done? Alrightly.<br />
+You should now find your server unique hostname and its port, it can be found in bottom of the Console page. It usually starts with <b>game#.falixserver.net:00000</b>. Found it? Select it and paste it in your Minecraft multiplayer.<br />
+<br />
+Connect to it and... Awesome, you are in! Have fun with your friends! Simple as that.";
