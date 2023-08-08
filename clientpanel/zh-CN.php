c87404596877b8cbe725de9ff94a0aa3a5075166
Jaydenha09 <98650905+Jaydenha09@users.noreply.github.com>
2023-08-08 08:28:55 +0800

    Update zh-CN.php
    Added zh-CN

--- clientpanel/zh-CN.php
+++ clientpanel/zh-CN.php
@@ -1,1 +1,270 @@
 
+<?php
+
+// 页面名称
+$lang['title']['mainpage_title'] ="查看服务器";       
+$lang['title']['mainpage_description'] ="查看和管理您的Minecraft服务器。";  
+
+$lang['title']['createserver_title'] ="创建服务器";
+$lang['title']['createserver_description']="创建您自己的Minecraft服务器。"; 
+
+$lang['title']['changeram_title']="更改RAM";       
+$lang['title']['changeram_description']="修改您服务器的资源。";
+
+$lang['title']['alwaysonline_title'] ="一直在线";       
+$lang['title']['alwaysonline_description']="在建设中。";
+
+$lang['title']['coins_title'] ="硬币";       
+$lang['title']['coins_description']="取得硬币并将其换成 Minecraft服务器提升。";
+
+// 导航栏  
+$lang['nav']['dashboard'] ="控制面板";
+$lang['nav']['coins'] ="硬币";        
+$lang['nav']['mainpage'] ="首页";      
+$lang['nav']['servers']="服务器";
+$lang['nav']['createserver'] ="创建服务器";       
+$lang['nav']['changeram'] ="更改RAM";
+$lang['nav']['alwaysonline']="一直在线";        
+$lang['nav']['soon'] ="即将";      
+$lang['nav']['account'] ="帐户";        
+$lang['nav']['logintopanel']="登录到面板";
+$lang['nav']['plandetails'] ="计划详情";
+$lang['nav']['billing']="计费";
+
+// 错误
+$lang['error']['login_is_required']="需要登录!";       
+$lang['error']['login_is_required_description']="登录后查看您的服务器。";
+$lang['error']['login_is_required_button']="登录";  
+
+$lang['error']['create_new_server'] ="创建您自己的服务器!";       
+$lang['error']['create_new_server_description']="您可以在几秒钟内创建您的第一个服务器。"; 
+$lang['error']['create_new_server_button']="创建服务器";
+
+$lang['error']['construction']="该页面正在建设中!";       
+$lang['error']['construction_description']="我们目前正在处理此事,同时请耐心等待。";
+
+//提示信息 
+$lang['notices']['premiumdiscount']="使用优惠码<b>UPGRADE10</b>提升您的计划至至高级和终极计划时,可获得<b>10%</b>减免达<b>6个月</b>";    
+$lang['notices']['premiumad'] ="使用Falix权限计划以最佳硬件托管您的服务器,附加优点包含24/7在线和广告免费网站,以低于市场价格价格。<a href='https://falixnodes.net/minecraft-server-hosting'>点击我</a>!";
+
+//小部件
+$lang['widgets']['panel_title'] ="您的面板详情";      
+$lang['widgets']['panel_ftp']="您的FTP密码";        
+$lang['widgets']['panel_resetpass']="密码错误?您可以在<a href='/profile/settings'>个人资料设置</a>重设密码。";
+$lang['widgets']['panel_loginbutton']="登录到面板";      
+$lang['widgets']['panel_closebutton']="关闭";      
+
+$lang['widgets']['changecpu_title']="更改CPU";       
+$lang['widgets']['changecpu_newcpu'] ="新的服务器CPU内核";        
+$lang['widgets']['changecpu_changebutton']="更改CPU";
+$lang['widgets']['changecpu_closebutton']="关闭";         
+
+$lang['widgets']['changedisk_title']="更改磁盘";      
+$lang['widgets']['changedisk_newcpu']="新的服务器磁盘空间(MB)";       
+$lang['widgets']['changedisk_changebutton']="更改磁盘";
+$lang['widgets']['changedisk_closebutton']="关闭";
+
+// 快捷链接      
+$lang['links']['quicklinks'] ="快捷链接";
+$lang['links']['quicklinks_description']="通过我们的快捷链接浏览控制面板";
+$lang['links']['createserver']="创建您自己的服务器";
+$lang['links']['createserver_description']="创建您自己的Minecraft服务器";
+$lang['links']['coinshop']="在Falix硬币商店购买";        
+$lang['links']['coinshop_description'] ="购买提升您Minecraft服务器";
+$lang['links']['coins']="免费赢取Falix硬币";       
+$lang['links']['coins_description']="通过赢取我们的虚拟货币提升您的服务器";       
+$lang['links']['discordsupport']="Discord支持";  
+$lang['links']['discordsupport_description']="向我们的社区提交您的问题";
+
+// 资源widget
+$lang['resources']['serversbalance']="服务器余额";  
+$lang['resources']['rambalance']="RAM余额";
+$lang['resources']['cpubalance'] ="CPU余额";
+$lang['resources']['diskbalance']="磁盘余额";
+$lang['resources']['databasesbalance']="数据库余额";        
+$lang['resources']['portsbalance']="端口余额";
+
+//登录页面
+$lang['auth']['login_title'] ="FalixNodes";
+$lang['auth']['login_description']="立即获取免费托管";
+$lang['auth']['have_an_account']="已有账号?";
+$lang['auth']['have_an_account_button']="登录";        
+$lang['auth']['dont_have_account_yet']="还没有账号?<br><a class='text-decoration-underline link-light' href='https://client.falixnodes.net/auth/register'>现在开始!</a>";
+$lang['auth']['useragreement']="阅读我们的<a class='text-decoration-underline text-white' href='https://falixnodes.net/user-agreement'>用户协议</a>";     
+$lang['auth']['loginform_title']="账户登录";      
+$lang['auth']['form_email']="电子邮件地址";
+$lang['auth']['form_password']="密码";
+$lang['auth']['form_confirmpassword']="确认密码";
+$lang['auth']['loginform_forgotpassword']="忘记密码?";
+$lang['auth']['loginform_button']="登录";
+$lang['auth']['oauthlogins']="或者使用";
+$lang['auth']['register_oauthlogins']="或者使用";  
+$lang['auth']['google']="谷歌";      
+$lang['auth']['discord']="Discord";
+$lang['auth']['registerform_title']="注册";
+$lang['auth']['registerform_button']="注册";
+$lang['auth']['registerform_tosaccept']="我接受<a href='https://falixnodes.net/user-agreement'>用户协议 </a>和<a href='https://falixnodes.net/privacy-policy'>隐私政策</a>";  
+$lang['auth']['registerform_marketingconsent']="若要收到我们最新优惠的营销电子邮件,请选中此框。";
+
+//index.php / 主页
+$lang['manage_servers']="使用我们友好的游戏面板控制您的服务器。";
+
+// 服务器表
+$lang['servers']="您的服务器";
+$lang['logintogamepanel']="登录到游戏面板";       
+$lang['logintogamepanel_button']="游戏面板";
+
+$lang['server']['name']="服务器名称";  
+$lang['server']['cpu'] ="CPU";        
+$lang['server']['ram']="RAM";
+$lang['server']['disk']="磁盘";
+$lang['server']['database']="数据库";
+$lang['server']['port'] ="端口";
+
+$lang['server']['gotoserver']="进入服务器";  
+$lang['server']['changecpu']="更改CPU";       
+$lang['server']['changeram'] ="更改RAM";
+$lang['server']['changedisk']="更改磁盘";
+$lang['server']['deleteserver']="删除服务器";  
+
+//创建服务器页面
+$lang['create']['createserver'] = "创建服务器";       
+$lang['create']['servername'] = "服务器名称";       
+$lang['create']['serverram'] = "服务器内存";        
+$lang['create']['servergame'] = "游戏";         
+$lang['create']['allgames'] = "所有支持的游戏";
+$lang['create']['selectgame'] = "选择游戏";        
+$lang['create']['minecrafteula'] = "我接受<a href='https://account.mojang.com/documents/minecraft_eula/'>《Minecraft最终用户许可协议》</a>"; 
+$lang['create']['createbutton'] = "创建";       
+$lang['create']['adblock'] = "((( 如果按钮在等待3-5秒后仍然不工作,请禁用广告拦截。 )))";
+
+// 更改内存页面     
+$lang['changeram']['title'] = "更改RAM";         
+$lang['changeram']['servername'] = "服务器名称";       
+$lang['changeram']['currentram'] = "当前RAM";
+$lang['changeram']['newram'] = "新RAM";     
+$lang['changeram']['changerambutton'] = "更改RAM";
+
+//硬币页面   
+$lang['coins']['earnfalixcoins'] = "赢取Falix硬币";
+$lang['coins']['resourcestore'] = "Falix硬币商店";  
+$lang['coins']['task'] = "任务";       
+$lang['coins']['reward'] = "奖励";
+$lang['coins']['earn'] = "赢取";       
+$lang['coins']['claim'] = "兑换";
+$lang['coins']['invitefriends'] = "邀请您的好友";       
+$lang['coins']['joinforrewards'] = "加入赢取奖励";
+$lang['coins']['invitereward'] = "每邀请一位好友1000 Falix硬币";
+$lang['coins']['joinreward'] = "加入每个服务器高达50 Falix硬币"
+
+// 资助者页面       
+$lang['affiliate']['title'] = "邀请一位好友,获得1000个Falix硬币";
+$lang['affiliate']['description'] ="邀请您的好友并在秒钟内获得奖励。";      
+$lang['affiliate']['seconddescription']="通过复制下面的网站链接并发送给您的好友来邀请您的好友。";
+$lang['affiliate']['footer'] ="一旦您邀请了好友,您可以<a href='https://client.falixnodes.net/affiliate/status'>查看您的推荐状况</a>,或者如果您有任何疑问,可以在我们的Discord服务器与我们联系。";
+
+//活动页面      
+$lang['activity']['title'] ="活动";
+$lang['activity']['id'] ="ID";
+$lang['activity']['action']="操作";
+$lang['activity']['description'] ="描述";     
+$lang['activity']['useragent']="用户代理";         
+$lang['activity']['ip'] ="IP地址";
+$lang['activity']['date'] ="日期";
+
+// 个人资料页面
+$lang['profile']['settings_title'] = "个人资料设置";
+$lang['profile']['firstname'] = "名字";  
+$lang['profile']['lastname'] = "姓氏";
+$lang['profile']['email'] = "电子邮件";       
+$lang['profile']['theme'] = "网站主题";       
+$lang['profile']['textcolor'] = "文本颜色";
+
+$lang['profile']['serversettings_title'] = "服务器设置";      
+$lang['profile']['externalserverstart'] = "外部服务器启动";   
+$lang['profile']['update'] = "更新";
+$lang['profile']['enable'] = "启用";
+$lang['profile']['disable'] = "禁用";
+
+$lang['profile']['emailpreference_title'] = "电子邮件首选项";      
+$lang['profile']['emailpreference'] = "更改您的电子邮件首选项";    
+
+$lang['profile']['changepassword_title'] = "更改密码";
+$lang['profile']['changepassword_notice'] = "NB! 如果您正在尝试更改游戏面板密码,请滚动到下面的另一个密码提示。";          
+$lang['profile']['oldpassword'] = "旧密码";      
+$lang['profile']['newpassword'] ="新密码";      
+$lang['profile']['confirmpassword'] = "确认密码";
+$lang['profile']['updatepassword'] = "更新密码";
+
+$lang['profile']['discord'] = "Discord";
+$lang['profile']['authorised'] = "授权为";
+$lang['profile']['linkdiscord'] = "链接Discord";
+$lang['profile']['unlinkdiscord'] = "断开Discord链接"; 
+$lang['profle']['notlinked'] = "您没有将Discord账号链接到该账户,要将Discord账户链接到您的账户,请授权您的Discord账户";
+
+$lang['profile']['dangerzone'] = "危险区";            
+$lang['profile']['changegamepanelpassword_title'] = "更改游戏面板密码";
+$lang['profile']['changegamepanelpassword_description'] = "一旦您更改游戏面板密码,旧密码将无法使用。";       
+$lang['profile']['changegamepanelpassword_button'] = "更改密码";
+$lang['profile']['deleteaccount_title'] = "删除此帐户";         
+$lang['profile']['deleteaccount_description'] = "一旦删除帐户,就无法恢复。请确保需要。";
+$lang['profile']['deleteaccount_button'] = "删除账户";
+
+// Falix硬币商店       
+$lang['store']['plan_expire_on'] = "您的计划将自动在以下时间续订";
+$lang['store']['month'] = "月";     
+$lang['store']['cancelplan'] = "取消计划";
+$lang['store']['purchase'] = "立即购买";
+$lang['store']['booster'] = "提升";  
+
+$lang['store']['booster1_servers'] = "最多创建3个服务器";
+$lang['store']['booster1_cpu'] = "每个服务器获得120% CPU性能";       
+$lang['store']['booster1_ram'] = "获得6GB内存";
+$lang['store']['booster1_disk'] = "每个服务器获得15GB磁盘";       
+$lang['store']['booster1_mysql'] = "每个服务器获得3个MySQL数据库";    
+$lang['store']['booster1_ports'] = "每个服务器获得2个额外端口";
+
+$lang['store']['booster2_servers'] = "最多创建4个服务器";
+$lang['store']['booster2_cpu'] = "每个服务器获得130% CPU性能";       
+$lang['store']['booster2_ram'] = "获得7GB内存";  
+$lang['store']['booster2_disk'] = "每个服务器获得15GB磁盘";
+$lang['store']['booster2_mysql'] = "每个服务器获得3个MySQL数据库";       
+$lang['store']['booster2_ports'] = "每个服务器获得2个额外端口";
+
+$lang['store']['booster3_servers'] = "最多创建5个服务器";       
+$lang['store']['booster3_cpu'] = "每个服务器获得140% CPU性能";
+$lang['store']['booster3_ram'] = "获得8GB内存";
+$lang['store']['booster3_disk'] = "每个服务器获得15GB磁盘";       
+$lang['store']['booster3_mysql'] = "每个服务器获得3个MySQL数据库";
+$lang['store']['booster3_ports'] = "每个服务器获得2个额外端口";
+
+// 教程
+$lang['tutorials']['createserver_stepone_title'] = "创建你的服务器";
+$lang['tutorials']['createserver_stepone'] = "第一次来这里？别担心！<br />
+通过创建你的第一台服务器开始使用Falix。导航到<a href='/create' target='_blank'>创建服务器页面</a>，并通过填写表单开始创建过程。<br />
+<br />
+<b>服务器名称：</b>给你的服务器取一个名字，比如'朋友服务器'或'我的服务器'。<br />
+<b>服务器RAM：</b>这可能有点令人困惑，但别担心！点击<a data-bs-toggle='offcanvas' data-bs-target='#RAMExplanation' aria-controls='RAMExplanation' style='text-decoration: underline dotted; cursor: pointer;' href='#'>这里</a>阅读我们的解释。<br />
+<br />
+填写完表单后，点击'创建'... 恭喜！你已经成功创建了自己的第一台服务器。导航到'管理你的服务器'以了解如何访问它。
+
+$lang['tutorials']['createserver_steptwo_title'] = "管理你的服务器";
+$lang['tutorials']['createserver_steptwo'] = "你已经创建了第一台服务器，接下来做什么？<br />
+非常简单，点击服务器表格右上角的'登录到游戏面板'按钮，在游戏面板中登录你的账户。<br />
+<br />
+然后，你将看到游戏面板界面。在界面中找到你的服务器并点击它。然后，请确保导航到导航栏中的'版本'选项卡。<br />
+<br />
+然后，你将被要求选择适合你的服务器的最合适的软件和版本。我们建议你从Paper开始，因为它是所有版本中最优化的软件。然后，请找到你的版本并选择它。<br />
+<br />
+完成上述步骤后，你的服务器已经安装好了。现在导航到你的服务器控制台，并通过点击'启动'按钮来启动服务器。<br />
+<br />
+太棒了，你的服务器现在正在运行！前往下一个教程，了解如何连接到它。
+
+$lang['tutorials']['createserver_stepthree_title'] = "连接到你的服务器";
+$lang['tutorials']['createserver_stepthree'] = "你的服务器正在运行，接下来做什么？<br />
+是时候在上面玩耍了！但是怎么做呢？<br />
+<br />
+使用你在游戏面板中选择的版本启动你的Minecraft客户端。准备好了吗？好的。<br />
+现在，你应该找到你服务器的唯一主机名和端口，它通常可以在控制台页面的底部找到。通常以<b>game#.falixserver.net:00000</b>开头。找到了吗？选择它并粘贴到你的Minecraft多人游戏中。<br />
+<br />
+连接上去... 太棒了，你已经成功进入了！和朋友一起玩得开心吧！就这么简单。
